09cd17c390ba407b389b7e18ad760cb2401b5513
Urrea <Surrea1411@gmail.com>
2023-09-16 02:33:40 -0500

    fefeee

--- Exercise18/Program.cs
+++ Exercise18/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 2, divisible = 0;
+Console.Write("Up to what number do you want to print primes? ");
+int finalNumber = int.Parse(Console.ReadLine());
+while (number <= finalNumber)
+{
+    for (int i = 1; i < number; i ++)
+    {
+        if (number % i ==0)
+        {
+            divisible++;
+        }
+        if (divisible > 2 )
+        {
+            break;
+        }
+    }
+    if (divisible == 2 )
+    {
+        Console.Write("{0,10:N0} is prime", number);
+    }
+    divisible = 0;
+    number++;
+    
+}
+Console.ReadKey();
+
